23abafad45b629379fc75a61c527c3338e9d67bd
Alexey Fedorov <waterlink000@gmail.com>
2011-02-08 21:58:03 +0200

    added debug messaging in main function 1.02

--- source/x.cpp
+++ source/x.cpp
@@ -6,6 +6,7 @@ while (1){
      int cxScreen, cyScreen ;
 
 	if (runhellobox() != IDOK) return 0;
+	else debugmsg("_start: user answered ok", 5);
 
      cxScreen = GetSystemMetrics (SM_CXSCREEN) ;
      cyScreen = GetSystemMetrics (SM_CYSCREEN) ;
